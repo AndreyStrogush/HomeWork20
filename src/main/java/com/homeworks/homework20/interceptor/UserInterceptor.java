774618b29964f6222c6b38ff0eab36fa4cef4700
Andrey Strogush <magistrogush@gmail.com>
2019-05-17 22:12:56 +0300

    new commit

---
+++ src/main/java/com/homeworks/homework20/interceptor/UserInterceptor.java
@@ -0,0 +1,35 @@
+package com.homeworks.homework20.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class UserInterceptor extends HandlerInterceptorAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(UserInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception{
+        logger.info("Client User-Agent: " + request.getHeader("User-Agent"));
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                Object handler, Exception ex) throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+    }
+
+}
---
+++ src/main/java/com/homeworks/homework20/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.homeworks.homework20.service;
+
+public interface EmailService {
+    void send(String to, String title, String body);
+}
