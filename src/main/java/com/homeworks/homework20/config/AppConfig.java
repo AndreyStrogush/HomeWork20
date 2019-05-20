46fba89e65581a0949dd2eb75523665b925503a2
Andrey Strogush <magistrogush@gmail.com>
2019-05-20 22:00:14 +0300

    fixes #3

--- src/main/java/com/homeworks/homework20/config/AppConfig.java
+++ src/main/java/com/homeworks/homework20/config/AppConfig.java
@@ -1,12 +1,14 @@
 package com.homeworks.homework20.config;
 
 import com.homeworks.homework20.interceptor.UserInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AppConfig implements WebMvcConfigurer {
     @Autowired
     private UserInterceptor userInterceptor;
--- src/main/java/com/homeworks/homework20/service/SchedulerService.java
+++ src/main/java/com/homeworks/homework20/service/SchedulerService.java
@@ -1,8 +1,10 @@
 package com.homeworks.homework20.service;
 
+import com.homeworks.homework20.beanpostprocessor.UserBeanPostProcessor;
 import com.homeworks.homework20.model.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class SchedulerService { 
+public class SchedulerService {
+    private static final Logger LOGGER  = LoggerFactory.getLogger(UserBeanPostProcessor.class);
+
     private final UserService userService;
     private final EmailService emailService;
 
@@ -23,17 +26,15 @@ public class SchedulerService {
         int month = date.getMonthValue();
         int day = date.getDayOfMonth();
         List<User> usersBirthdays = userService.findAllUsersByBirthDay(month, day);
-        if (!usersBirthdays.isEmpty()) {
-            usersBirthdays.forEach(user -> {
-                try {
-                    String message = "Happy Birthday " + user.getName() + "!";
-                    emailService.send(user.getEmail(), "Happy Birthday!", message);
-                    log.info("Email have been sent. User " + user.getId() + " Date: " + date);
-                } catch (Exception e) {
-                    log.error("Email can't be sent. User " + user.getId() + " Date: " + date);
-                    log.error("Email can't be sent. " + e.getMessage());
-                }
-            });
-        }
+
+        usersBirthdays.forEach(user -> {
+            try {
+                String message = "Happy Birthday " + user.getName() + "!";
+                emailService.send(user.getEmail(), "Happy Birthday!", message);
+                LOGGER.info("Email have been sent. User " + user.getId() + " Date: " + date);
+            } catch (Exception e) {
+                LOGGER.error("Email can't be sent. " + e.getMessage());
+            }
+        });
     }
 }
